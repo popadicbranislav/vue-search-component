c2960ca68e6a3596c56749b17fec707438f72365
popadicbranislav <popadicbranislav@gmail.com>
2022-01-16 21:26:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-lgp6rm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-lgp6rm)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import SearchComponent from './components/SearchComponent.vue';
+</script>
+
+<template>
+  <SearchComponent msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  margin: auto;
+  box-shadow: 0 0 5rem rgba(0, 0, 0, 0.3);
+  padding: 2rem;
+}
+</style>
